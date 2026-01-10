529d4efa6a1daa9ff75696ca301bcee4a90635b2
Raymond Matteson <raymatt@gmail.com>
2026-01-10 16:34:15 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-8198471998-f4406",
+  "appId": "1:497973290484:web:23ebd21f5c6a03e56745a1",
+  "apiKey": "AIzaSyCUwaCMyGrNd6nwM8KR0wTgShKXXSGej70",
+  "authDomain": "studio-8198471998-f4406.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "497973290484"
+};
