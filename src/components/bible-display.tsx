e837ed3684fafa98e4294e633e8752db0dc03f9a
Raymond Matteson <raymatt@gmail.com>
2026-07-20 16:09:42 +0000

    Tighten up the space some between verses.

--- src/components/bible-display.tsx
+++ src/components/bible-display.tsx
@@ -308,7 +308,7 @@ function VerseComponent({
     );
 
     return (
-        <div data-verse-number={verse.number} className={cn("block mb-8 md:mb-10", textClasses)}>
+        <div data-verse-number={verse.number} className={cn("block mb-4 md:mb-6", textClasses)}>
             <sup 
                 className="font-headline font-bold text-primary mr-2 select-none cursor-pointer align-baseline"
                 onClick={handleVerseNumberClick}
